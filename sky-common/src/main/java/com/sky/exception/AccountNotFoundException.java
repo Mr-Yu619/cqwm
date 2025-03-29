e59c8bdb9f8154ba7368b42865f34feedd2cfecf
yushuo619 <yushuo619@noreply.gitcode.com>
2025-03-29 14:26:26 +0800

    各种异常

---
+++ sky-common/src/main/java/com/sky/exception/AccountNotFoundException.java
@@ -0,0 +1,9 @@
+package com.sky.exception;
+
+public class AccountNotFoundException extends BaseException {
+    public AccountNotFoundException() {
+    }
+    public AccountNotFoundException(String message) {
+        super(message);
+    }
+}
