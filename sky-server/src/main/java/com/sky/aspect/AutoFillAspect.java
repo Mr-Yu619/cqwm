37f9ee817849039ccbf8d662e7f3bb9412ee5cab
yushuo619 <yushuo619@noreply.gitcode.com>
2025-03-29 16:35:25 +0800

    定义自动注入切面

---
+++ sky-server/src/main/java/com/sky/aspect/AutoFillAspect.java
@@ -0,0 +1,95 @@
+package com.sky.aspect;
+
+import com.sky.annotation.AutoFill;
+import com.sky.constant.AutoFillConstant;
+import com.sky.context.BaseContext;
+import com.sky.enumeration.Operationtype;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+import java.time.LocalDateTime;
+
+/**
+ * 自定义切面，实现公共字段自动填充处理逻辑
+ */
+@Aspect
+@Component
+@Slf4j
+public class AutoFillAspect {
+    /**
+     * 切入点
+     */
+    @Pointcut("execution(* com.sky.mapper.*.*(..)) && @annotation(com.sky.annotation.AutoFill)")
+    public void autoFillPointCut(){}
+
+    /**
+     * 前置通知，在通知中进行公共字段的赋值
+     */
+    @Before("autoFillPointCut()")
+    public void autoFill(JoinPoint joinPoint){
+        log.info("开始进行公共字段自动填充");
+
+        // 获取到当前被拦截的方法上的数据库操作类型
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature(); // 方法签名对象
+        AutoFill autoFill = signature.getMethod().getAnnotation(AutoFill.class);
+        Operationtype operationtype = autoFill.value();
+
+        //获取到当前被拦截的方法的参数--实体对象
+        Object[] args = joinPoint.getArgs();
+        if(args==null||args.length==0){
+            return;
+        }
+
+        Object entity = args[0];
+
+        // 准备赋值的数据
+        LocalDateTime now = LocalDateTime.now();
+        Long currentId = BaseContext.getCurrentId();
+
+        //根据当前不同的操作类型，为对应的属性通过反射来赋值
+        if(operationtype == Operationtype.INSERT){
+            try{
+                /**
+                 * 为什么在这段代码里不直接用entity对象调用方法，而是用反射？
+                 *
+                 * 切面是通用的，可能被应用到不同的Mapper方法上，
+                 * 而这些方法可能操作不同的实体类。如果直接调用entity的方法
+                 * 那么就需要在切面代码中知道所有可能的实体类型及其方法
+                 * 这在动态情况下是不可能的。
+                 *
+                 * 需要提前知道 entity 的具体类型（如 User、Order 等），
+                 * 但切面代码是与具体实体类解耦的，
+                 * 无法在编译时确定 entity 的类型。
+                 */
+                Method setCreateTime = entity.getClass().getDeclaredMethod(AutoFillConstant.SET_CREATE_TIME, LocalDateTime.class);
+                Method setCreateUser = entity.getClass().getDeclaredMethod(AutoFillConstant.SET_CREATE_USER,Long.class);
+                Method setUpdateTime = entity.getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_TIME, LocalDateTime.class);
+                Method setUpdateUser = entity.getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_USER, Long.class);
+
+                //通过反射为对象属性赋值
+                setCreateTime.invoke(entity, now);
+                setCreateUser.invoke(entity, currentId);
+                setUpdateTime.invoke(entity, now);
+                setUpdateUser.invoke(entity, currentId);
+            } catch(Exception e){
+                e.printStackTrace();
+            }
+        } else if(operationtype == Operationtype.UPDATE){
+            try{
+                Method setUpdateTime = entity.getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_TIME, LocalDateTime.class);
+                Method setUpadteUser = entity.getClass().getDeclaredMethod(AutoFillConstant.SET_CREATE_USER, Long.class);
+
+                setUpdateTime.invoke(entity);
+                setUpadteUser.invoke(entity);
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
